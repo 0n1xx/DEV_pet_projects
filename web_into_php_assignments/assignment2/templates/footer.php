acde057082524c30ad254d97c3346c8577dc542d
Vlad Sakharov <vladsakharov@Vlads-MacBook-Air.local>
2025-09-26 11:05:54 -0400

    adds the second assignment

---
+++ web_into_php_assignments/assignment2/templates/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div>
+        <a href="header.php"> <img src="logo_img/logo.png" alt="header logo"></a>
+    </div>
+    <div>
+        <p>Contact us: beaver_pizza@gmail.combeaver_pizza@gmail.com | +1 (705) 123-4567</p>
+        <p>123 Anne St, Barrie, Canada</p>
+        <p>© 2025 Beaver Pizza. All rights reserved.</p>
+    </div>
+</footer>
+</main>
+</body>
+</html>
---
+++ web_into_php_assignments/assignment2/templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- In the head element, we add our metadata -->
+    <!-- Declare our character set -->
+    <meta charset="utf-8">
+    <!-- This measures the viewport of the device we are using -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- This will describe our pages content, this should be different
+      on each page -->
+    <meta name="description" content="The page for ordering pizza">
+    <!-- This will tell the search engine to index the site or not -->
+    <meta name="robots" content="noindex, nofollow">
+    <!-- This is our title tag  -->
+    <title> Ordering pizza </title>
+    <!-- This is the element that connects css file to our html_css_js_introduction file -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Using a special google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Varela&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- Creating a header with the nav bar -->
+<header>
+    <div class="logo-container">
+        <a href="header.php">
+            <img src="logo_img/logo.png" alt="header logo">
+        </a>
+    </div>
+    <nav>
+        <ul class="big_screens_li">
+            <li><a href="#">Main</a></li>
+            <li><a href="#">Locations</a></li>
+            <li><a href="#">Contact Page</a></li>
+            <li><a href="#">About Page</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <div>
+        <h1>Choose the pizza options</h1>
+    </div>
